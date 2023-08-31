e7c109bd38c97ef09d571dbe7ebbff724c011b22
francescotroiano98 <francesco-troiano-98@libero.it>
2023-08-31 16:43:35 +0200

    Added loading animation before all projects are loaded

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -3,8 +3,13 @@
         <h1 class="main-title">
             Latest Projects:
         </h1>
+        <div v-if="isLoading" class="loading-overlay">
+            <div class="loading-spinner"></div>
+        </div>
 
-        <nav class="pagination">
+        <div v-else class="projects">
+            <!-- ... Elenco dei progetti ... -->
+            <nav class="pagination">
             <li class="prev" @click="prevPage" v-if="prevPageUrl">
                 Previous
             </li>
@@ -32,6 +37,10 @@
                 </p>
             </div>
         </div>
+
+
+        </div>
+        
     </div>
 </template>
 <script>
@@ -49,12 +58,17 @@ export default{
             nextPageUrl : '',
             currentPageNo: '',
             prevPageUrl : '',
+
+            isLoading: true,
         }
     },
 
     methods: {
         getProjects(apiUrl = this.apiUrl){
             // recuper i miei project e popolo la variabile projects
+
+            this.isLoading = true;
+
             axios.get(apiUrl)
             .then((response) => {
                 console.log(response)
@@ -65,6 +79,9 @@ export default{
             .catch(function (error) {
                 console.log(error);
             })
+            .finally(() => {
+                this.isLoading = false; // Fine del caricamento
+            });
         },
 
         nextPage(){
@@ -154,5 +171,33 @@ h1.main-title{
             }
         }
     }
+
+
+    .loading-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+}
+
+.loading-spinner {
+    border: 4px solid rgba(230, 0, 0, 0.3);
+    border-top: 4px solid #ffffff;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
 
 </style>
