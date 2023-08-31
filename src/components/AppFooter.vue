068d30b786e679c96511651554520e04e358eea2
francescotroiano98 <francesco-troiano-98@libero.it>
2023-08-31 16:06:01 +0200

    added api call in the projects.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,18 +1,72 @@
 <template>
-    <div>
-        <h1>
-            Footer
-        </h1>
-        
-    </div>
+
+        <footer>
+            <nav>
+                <ul>
+                    <li v-for="link in links">
+                        <router-link :to="{ name: link.route }">
+                            {{ link.name }}
+                        </router-link>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
 </template>
 <script>
 export default{
     name: 'AppFooter',
+
+    data() {
+        return {
+            links : [
+                {
+                    route: 'homepage',
+                    name: 'Homepage'
+                },
+                {
+                    route: 'about',
+                    name: 'About Us'
+                },
+                {
+                    route: 'projects',
+                    name: 'Projects'
+                },
+            ]
+        }
+    },
 }
 
 
 </script>
 <style lang="scss">
+footer{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    nav{
+        width: 100%;
+        padding: .5rem 4rem;
+        background-color: rgb(0, 0, 0);
+        color: rgb(255, 255, 255);
+
+        ul{
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            font-size: 1rem;
+            list-style-type: none;
+
+            li{
+                margin-right: 3rem;
+                a {
+                    text-decoration: none;
+                    color: rgb(255, 255, 255);
+                }
+            }
+        }
+    }
+}
 
 </style>
